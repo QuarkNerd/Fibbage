0cc6e52fcc83248484c80701fbf3eb7272f0a95e
QuarkNerd <Gurveer.S.Arora@gmail.com>
2024-03-31 20:42:34 +0000

    Deploying to gh-pages from @ QuarkNerd/Fibbage@4e163aa8d986aac3e9c3b30a7b6f849ca8e24bb3 🚀

---
+++ _app/immutable/entry/start.49rNkgHU.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.IlB8_IMH.js";export{t as start};
---
+++ _app/immutable/nodes/2.qMRi8mkl.js
@@ -0,0 +1,15 @@
+import{s as h,n as m}from"../chunks/scheduler.bGroypw6.js";import{S as u,i as p,n as f,s as d,e as g,o as v,f as w,c as y,k as _,p as b,q as k,g as i,l as $,m as x,d as l,r as q}from"../chunks/index.ZiHq4h1H.js";import{H}from"../chunks/SubmitText.svelte_svelte_type_style_lang.bXODe_Eg.js";/* empty css                      */function T(c){let t,s,o,r=`Welcome to Fibbage with custom questions. In this game you try to trick your friends while finding
+	the real answer yourselves.
+
+	<h3 class="svelte-k1wkez">Join a game</h3>
+	To create a new game click<a href="./create-game">here</a>, to join an existing game ask someone
+	to send you the link.
+
+	<h3 class="svelte-k1wkez">How to play?</h3> <div class="box">In each round you are presented with a question.
+		<br/> <br/>
+		The first thing to do is to create a convincing lie. If you know the real answer, don&#39;t use it or
+		something too close to it as that volates the spirit of the game.
+		<br/> <br/>
+		Then once everyone has submitted the lie, choose what you think is the correct option.
+		<br/> <br/>
+		You will earn points for getting the question right and for everyone who chooses your answer.</div>`,a;return t=new H({props:{header:"Fibbage"}}),{c(){f(t.$$.fragment),s=d(),o=g("div"),o.innerHTML=r,this.h()},l(e){v(t.$$.fragment,e),s=w(e),o=y(e,"DIV",{class:!0,"data-svelte-h":!0}),_(o)!=="svelte-1kmuu58"&&(o.innerHTML=r),this.h()},h(){b(o,"class","main")},m(e,n){k(t,e,n),i(e,s,n),i(e,o,n),a=!0},p:m,i(e){a||($(t.$$.fragment,e),a=!0)},o(e){x(t.$$.fragment,e),a=!1},d(e){e&&(l(s),l(o)),q(t,e)}}}class L extends u{constructor(t){super(),p(this,t,null,T,h,{})}}export{L as component};
